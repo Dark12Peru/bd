7cf7c2e698febcf4612357828708358c9eeb7cb0
LuisilloElPillo12 <108873299+Dark12Peru@users.noreply.github.com>
2024-07-28 12:56:18 -0500

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,239 @@
+let allMusic = [
+
+    {
+        name: "Me Porto Bonito",
+        artist: "Bad Bunny (ft. Chencho Corleone)",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Ojitos Lindos",
+        artist: "Bad Bunny (ft. Bomba Estéreo)",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Party",
+        artist: "Bad Bunny (ft. Rauw Alejandro)",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Tarot",
+        artist: "Bad Bunny (ft. Jhay Cortez)",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Me Fui de Vacaciones",
+        artist: "Bad Bunny",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Yo No Soy Celoso",
+        artist: "Bad Bunny",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "Después de la Playa",
+        artist: "Bad Bunny",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Un Ratito",
+        artist: "Bad Bunny",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Otro Atardecer",
+        artist: "Bad Bunny (ft. The Marias)",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Efecto",
+        artist: "Bad Bunny",
+        img: "music-10",
+        src: "music-10"
+    },
+    {
+        name: "Aguacero",
+        artist: "Bad Bunny",
+        img: "music-11",
+        src: "music-11"
+    },
+    {
+        name: "Agosto",
+        artist: "Bad Bunny",
+        img: "music-12",
+        src: "music-12"
+    },
+    {
+        name: "Un Coco",
+        artist: "Bad Bunny",
+        img: "music-13",
+        src: "music-13"
+    },
+    {
+        name: "Neverita",
+        artist: "Bad Bunny",
+        img: "music-14",
+        src: "music-14"
+    },
+    {
+        name: "Andrea",
+        artist: "Bad Bunny (ft. Buscabulla)",
+        img: "music-15",
+        src: "music-15"
+    },
+    {
+        name: "Enséñame a Bailar",
+        artist: "Bad Bunny",
+        img: "music-16",
+        src: "music-16"
+    },
+    {
+        name: "Un Verano Sin Ti",
+        artist: "Bad Bunny",
+        img: "music-17",
+        src: "music-17"
+    },
+    {
+        name: "Dos Mil 16",
+        artist: "Bad Bunny",
+        img: "music-18",
+        src: "music-18"
+    },
+    {
+        name: "La Corriente",
+        artist: "Bad Bunny (ft. Tony Dize))",
+        img: "music-19",
+        src: "music-19"
+    },
+    {
+        name: "Moscow Mule",
+        artist: "Bad Bunny",
+        img: "music-20",
+        src: "music-20"
+    },
+    {
+        name: "El Apagón",
+        artist: "Bad Bunny",
+        img: "music-21",
+        src: "music-21"
+    },
+    {
+        name: "TE DESEO LO MEJOR",
+        artist: "Bad Bunny",
+        img: "music-22",
+        src: "music-22"
+    },
+    {
+        name: "De Museo",
+        artist: "Bad Bunny",
+        img: "music-23",
+        src: "music-23"
+    },
+    {
+        name: "Yonaguni",
+        artist: "Bad Bunny",
+        img: "music-24",
+        src: "music-24"
+    },
+    {
+        name: "LA NOCHE DE ANOCHE",
+        artist: "BAD BUNNY x ROSALÍA",
+        img: "music-25",
+        src: "music-25"
+    },
+    {
+        name: "HOY COBRÉ",
+        artist: "Bad Bunny",
+        img: "music-26",
+        src: "music-26"
+    },
+    {
+        name: "DÁKITI",
+        artist: "BAD BUNNY x JHAY CORTEZ",
+        img: "music-27",
+        src: "music-27"
+    },
+    {
+        name: "YO VISTO ASÍ",
+        artist: "Bad Bunny",
+        img: "music-28",
+        src: "music-28"
+    },
+    {
+        name: "UNA VEZ",
+        artist: "BAD BUNNY x MORA",
+        img: "music-29",
+        src: "music-29"
+    },
+    {
+        name: "LA DIFÍCIL",
+        artist: "Bad Bunny",
+        img: "music-30",
+        src: "music-30"
+    },
+    {
+        name: "SI VEO A TU MAMÁ",
+        artist: "Bad Bunny",
+        img: "music-31",
+        src: "music-31"
+    },
+    {
+        name: "LA SANTA",
+        artist: "BAD BUNNY x DADDY YANKEEL",
+        img: "music-32",
+        src: "music-32"
+    },
+    {
+        name: "¿QUIÉN TÚ ERES?",
+        artist: "Bad Bunny",
+        img: "music-33",
+        src: "music-33"
+    },
+    {
+        name: "YO LE LLEGO",
+        artist: "J BALVIN x BAD BUNNY",
+        img: "music-34",
+        src: "music-34"
+    },
+    {
+        name: "LA CANCIÓN",
+        artist: "J BALVIN x BAD BUNNY",
+        img: "music-35",
+        src: "music-35"
+    },
+    {
+        name: "VETE",
+        artist: "Bad Bunny",
+        img: "music-36",
+        src: "music-36"
+    },
+    {
+        name: "CALLAÍTA ",
+        artist: "Bad Bunny",
+        img: "music-37",
+        src: "music-37"
+    },
+    {
+        name: "SI ESTUVIÉSEMOS JUNTOS",
+        artist: "Bad Bunny",
+        img: "music-38",
+        src: "music-38"
+    },
+    {
+        name: "MÍA",
+        artist: "BAD BUNNY x DRAKE",
+        img: "music-39",
+        src: "music-39"
+    },
+
+    
+    ];
